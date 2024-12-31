bff00e375a6f69a1c9bb05ef11180a832f53948b
Vahid Mohsseni <vahidmohsseni@users.noreply.github.com>
2024-12-31 11:17:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,38 @@ echo "hello run.sh"
 
 touch symb2
 
-curl https://ash-speed.hetzner.com/10GB.bin
+sleep 5
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+
+sleep 5
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+
+sleep 5
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+sleep 5
+
+curl https://ash-speed.hetzner.com/100MB.bin
+
+
+
 
 chmod 777 symb2
 
